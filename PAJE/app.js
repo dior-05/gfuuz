d8ee2097f4cc22480af276ff6b28cfddc90686cc
dior-05 <dioryuldoshev1@gmail.com>
2024-12-19 13:57:14 +0500

    javohir4

---
+++ PAJE/app.js
@@ -0,0 +1,13 @@
+let button = document.querySelector("#button")
+
+
+let modal =document.querySelector(".modal")
+
+button.addEventListener("click",()=>{
+    modal.classList.toggle("show")
+
+})
+
+
+
+
